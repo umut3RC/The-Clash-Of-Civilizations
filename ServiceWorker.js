aa1b4f24a3eb2c91d56a7176f393ab9bac895e03
umut3RC <maeumut@gmail.com>
2025-05-29 20:30:05 +0300

    added game export

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Umter Studios-the_clash_of_civilizations-0.1";
+const contentToCache = [
+    "Build/export-the_clash_of_civilizations.loader.js",
+    "Build/export-the_clash_of_civilizations.framework.js",
+    "Build/export-the_clash_of_civilizations.data",
+    "Build/export-the_clash_of_civilizations.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
