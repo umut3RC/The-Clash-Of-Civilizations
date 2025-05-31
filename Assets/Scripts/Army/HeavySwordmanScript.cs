25dd5a62d914cf35787955f6b41578bdb79b5106
umut3RC <maeumut@gmail.com>
2025-05-31 17:33:04 +0300

    added army spawn mechanic

---
+++ Assets/Scripts/Army/HeavySwordmanScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeavySwordmanScript : MonoBehaviour
+{
+	[SerializeField] private int amount = 10;
+
+	public int GetAmount()
+	{
+		return (amount);
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 public class PlayerScript : MonoBehaviour
 {
 	public GameObject spawnTargetVisulazer;
 	public LayerMask raycastLayer;
+	public GameObject testArmyPrefab;
 	[SerializeField] int totalCoins = 0;
 	[SerializeField] GameObject totalCoinText;
 
 	private float coinTimer = 0f;
 
 	bool selectedSpawn = false;
-	
+	string lastTargetArmyName = "";
+	GameObject lastTargetArmy;
 
 	void Start()
 	{
 		SetCoin(100);
+		spawnTargetVisulazer.SetActive(false);
 	}
 
 	void Update()
@@ -37,11 +39,14 @@ public class PlayerScript : MonoBehaviour
 			if (Physics.Raycast(ray, out RaycastHit hit, 100f, raycastLayer))
 			{
 				spawnTargetVisulazer.transform.position = hit.point;
+				if (lastTargetArmy != null)
+				{
+					lastTargetArmy.transform.position = spawnTargetVisulazer.transform.position;
+				}
 			}
 			if (Input.GetMouseButtonDown(0))
 			{
-				Debug.Log("Spawnlanik gari");
-				SelectArmy();
+				SpawnArmy();
 			}
 		}
 	}
@@ -57,8 +62,28 @@ public class PlayerScript : MonoBehaviour
 		totalCoins = c;
 		totalCoinText.GetComponent<TextMeshProUGUI>().text = totalCoins.ToString();
 	}
-	public void SelectArmy()
+	public void SelectArmy(string targetArmy)
 	{
-		selectedSpawn = !selectedSpawn;
+		lastTargetArmyName = targetArmy;
+		selectedSpawn = true;
+		spawnTargetVisulazer.SetActive(true);
+		if (selectedSpawn && lastTargetArmyName != null)
+		{
+			lastTargetArmy = Instantiate(testArmyPrefab);
+			int _amount = lastTargetArmy.GetComponent<HeavySwordmanScript>().GetAmount();
+			if (_amount >= totalCoins)
+			{
+				Destroy(lastTargetArmy);
+				selectedSpawn = false;
+				spawnTargetVisulazer.SetActive(false);
+			}
+		}
+	}
+
+	private void SpawnArmy()
+	{
+		selectedSpawn = false;
+		spawnTargetVisulazer.SetActive(false);
+		lastTargetArmy = null;
 	}
 }
